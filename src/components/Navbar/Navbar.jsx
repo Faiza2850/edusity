45f83248ccb35f1d9ecb2044586d80b2ab2825d0
Faiza2850 <saffafatima06@gmail.com>
2024-09-01 13:55:29 +0500

    second commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+export const Navbar = () => {
+  return (
+    <>
+    <nav className='container'>
+        <img  src={logo} alt='' className='logo'/>
+        <ul className='nav-items'>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About Us</li>
+            <li>Testimonials</li>
+            <li>Campus</li>
+        </ul>
+    </nav>
+    </>
+  )
+}
